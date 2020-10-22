fb4890e66d7ef97b47523442659e161abd82cc50
Kentaro Iwata <k3ntar01wata@gmail.com>
2020-10-22 23:05:38 +0900

    fixed deployment errors

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,7 +4,7 @@ const nodeExternals = require('webpack-node-externals')
 module.exports = {
     // モード値を production に設定すると最適化された状態で、
     // development に設定するとソースマップ有効でJSファイルが出力される
-    mode: 'development',
+    mode: 'production',
     entry: 'src/index.ts',
 
     // ファイルの出力設定
